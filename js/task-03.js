b8da5969f2dedf4de37df9267cca2170865f8350
wvlasenko <44614847+wvlasenko@users.noreply.github.com>
2020-07-04 12:56:04 +0300

    Add task 03

---
+++ js/task-03.js
@@ -0,0 +1,12 @@
+"use strict";
+const ADMIN_PASSWORD = "jqueryismyjam";
+let message;
+let userInput = prompt("Введите пароль");
+if (userInput === null) {
+  message = "Отменено пользователем!";
+} else if (userInput === ADMIN_PASSWORD) {
+  message = "Добро пожаловать!";
+} else {
+  message = "Доступ запрещен, неверный пароль!";
+}
+alert(message);

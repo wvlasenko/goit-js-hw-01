aeab662b5fe7b49c77365af6e1e9b54973f4f078
wvlasenko <44614847+wvlasenko@users.noreply.github.com>
2020-07-04 18:45:47 +0300

    Add task 06

---
+++ js/task-06.js
@@ -0,0 +1,18 @@
+"use strict";
+let total = 0;
+let userInput;
+do {
+  let userInput = prompt("введи число");
+  if (userInput === null) {
+    // alert("Отменено пользователем");
+    break;
+  }
+  userInput = Number(userInput);
+  const notANumber = Number.isNaN(userInput);
+  if (notANumber) {
+    alert("Было введено не число, попробуйте еще раз");
+    continue;
+  }
+  total += userInput;
+} while (true);
+alert(`Общая сумма чисел равна  ${total}`);

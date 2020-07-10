bf29a739dd088e4ead0328b01c2d9908c3025b8b
wvlasenko <44614847+wvlasenko@users.noreply.github.com>
2020-07-10 11:00:00 +0300

    Add alt realize

--- js/task-05.js
+++ js/task-05.js
@@ -2,49 +2,92 @@
 let coastOfDelivery = 0;
 let countryOfDelivery;
 let countryName;
-while (true) {
-  countryOfDelivery = prompt("Введите название страны для оформления доставки");
-  if (countryOfDelivery === null) {
-    alert("Отменено пользователем!");
-    break;
-  } else if (countryOfDelivery.length === 0) {
-    alert("Вы не ввели название страны");
-    continue;
-  } else {
-    countryName = countryOfDelivery.toLowerCase();
-  }
+let countryNameToSpell;
+let rezultMessage;
+
+countryOfDelivery = prompt("Введите название страны для оформления доставки");
+if (countryOfDelivery === null) {
+  rezultMessage = "Отменено пользователем!";
+} else if (countryOfDelivery.length === 0) {
+  rezultMessage = "Вы не ввели название страны";
+} else {
+  countryName = countryOfDelivery.toLowerCase();
+  countryNameToSpell = countryName[0].toUpperCase() + countryName.substr(1);
+}
+if (countryName) {
   switch (countryName) {
     case "китай":
       coastOfDelivery = 100;
-      alert(
-        `Доставка в ${countryName} будет стоить ${coastOfDelivery} кредитов.`
-      );
       break;
     case "чили":
       coastOfDelivery = 250;
-      alert(
-        `Доставка в ${countryName} будет стоить ${coastOfDelivery} кредитов.`
-      );
       break;
     case "австралия":
       coastOfDelivery = 170;
-      alert(
-        `Доставка в ${countryName} будет стоить ${coastOfDelivery} кредитов.`
-      );
       break;
     case "индия":
       coastOfDelivery = 80;
-      alert(
-        `Доставка в ${countryName} будет стоить ${coastOfDelivery} кредитов.`
-      );
       break;
     case "ямайка":
       coastOfDelivery = 120;
-      alert(
-        `Доставка в ${countryName} будет стоить ${coastOfDelivery} кредитов.`
-      );
       break;
     default:
-      alert("В вашей стране доставка не доступна");
+      rezultMessage = "В вашей стране доставка не доступна";
+      break;
+  }
+  if (coastOfDelivery != 0) {
+    rezultMessage = `Доставка в ${countryNameToSpell} будет стоить ${coastOfDelivery} кредитов.`;
   }
 }
+alert(rezultMessage);
+
+// Alternativ realize
+// let coastOfDelivery = 0;
+// let countryOfDelivery;
+// let countryName;
+// while (true) {
+//   countryOfDelivery = prompt("Введите название страны для оформления доставки");
+//   if (countryOfDelivery === null) {
+//     alert("Отменено пользователем!");
+//     break;
+//   } else if (countryOfDelivery.length === 0) {
+//     alert("Вы не ввели название страны");
+//     continue;
+//   } else {
+//     countryName = countryOfDelivery.toLowerCase();
+//   }
+//   switch (countryName) {
+//     case "китай":
+//       coastOfDelivery = 100;
+//       alert(
+//         `Доставка в ${countryName} будет стоить ${coastOfDelivery} кредитов.`
+//       );
+//       break;
+//     case "чили":
+//       coastOfDelivery = 250;
+//       alert(
+//         `Доставка в ${countryName} будет стоить ${coastOfDelivery} кредитов.`
+//       );
+//       break;
+//     case "австралия":
+//       coastOfDelivery = 170;
+//       alert(
+//         `Доставка в ${countryName} будет стоить ${coastOfDelivery} кредитов.`
+//       );
+//       break;
+//     case "индия":
+//       coastOfDelivery = 80;
+//       alert(
+//         `Доставка в ${countryName} будет стоить ${coastOfDelivery} кредитов.`
+//       );
+//       break;
+//     case "ямайка":
+//       coastOfDelivery = 120;
+//       alert(
+//         `Доставка в ${countryName} будет стоить ${coastOfDelivery} кредитов.`
+//       );
+//       break;
+//     default:
+//       alert("В вашей стране доставка не доступна");
+//   }
+// }
